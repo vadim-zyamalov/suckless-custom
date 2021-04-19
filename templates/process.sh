33a57b101c9d604ecbc7428d3b3262bba3b3038e
Vadim Zyamalov <zyamalov.ve@gmail.com>
2021-04-19 23:19:39 +0300

    U: processing script updated

--- templates/process.sh
+++ templates/process.sh
@@ -1,28 +1,29 @@
 #!/usr/bin/env sh
 
+[ ! "$1" ] && echo "Input needed: enter any program name!" && exit
+
 cur_path="$(pwd)"
 pkg_path="/home/zyamalov/git/void-packages"
 
-for d in *; do
-    if [ -d "$d" ]; then
-        echo "$d : prepare"
-        config=$(/usr/bin/ls "../sources/$d/config.h/" 2> /dev/null | grep -e "\.h$" | sort -r | head -n 1)
-        patchf=$(/usr/bin/ls "../sources/$d/patches/" 2> /dev/null | grep -e "\.diff$" | sort -r | head -n 1)
-        # Update files (if any)
-        [ -f "$d/files/config.h" ] && [ ! -z "$config" ] && echo "$d : config.h" && rm "$d/files/config.h" && cp "../sources/$d/config.h/$config" "$d/files/config.h"
-        [ -f "$d/files/patch.diff" ] && [ ! -z "$patchf" ] && echo "$d : patch.diff" && rm "$d/files/patch.diff" && cp "../sources/$d/patches/$patchf" "$d/files/patch.diff"
-        rm -r "$pkg_path/srcpkgs/$d"
-        cp -r "$d" "$pkg_path/srcpkgs/"
-    fi
-done
+while [ "$1" ]; do
+    if [ -d "$1" ]; then
+        echo "$1 : prepare"
 
-rm "$pkg_path/hostdir/binpkgs/"*
+        config=$(/usr/bin/ls "../sources/$1/config.h/" 2> /dev/null | grep -e "\.h$" | sort -r | head -n 1)
+        patchf=$(/usr/bin/ls "../sources/$1/patches/" 2> /dev/null | grep -e "\.diff$" | sort -r | head -n 1)
 
-for d in *; do
-    if [ -d "$d" ]; then
+        [ -f "$d/files/config.h" ] && [ ! -z "$config" ] && echo "$1 : config.h" && rm "$1/files/config.h" && cp "../sources/$1/config.h/$config" "$1/files/config.h"
+        [ -f "$d/files/patch.diff" ] && [ ! -z "$patchf" ] && echo "$1 : patch.diff" && rm "$1/files/patch.diff" && cp "../sources/$1/patches/$patchf" "$1/files/patch.diff"
+
+        rm -r "$pkg_path/srcpkgs/$1"
+        cp -r "$1" "$pkg_path/srcpkgs/"
+
+        del=$(/usr/bin/ls "$pkg_path/hostdir/binpkgs" | grep -e "^$1-[0-9\.]\+_[0-9]\+.*\.xbps")
+        [ ! -z "$del" ] && rm "$pkg_path/hostdir/binpkgs/$del"
+
         cd "$pkg_path"
-        ./xbps-src pkg "$d"
+        ./xbps-src pkg "$1"
         cd "$cur_path"
     fi
+    shift
 done
-
