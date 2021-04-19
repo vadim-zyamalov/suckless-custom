060714fe4f504469ee76c84839ca5ac27a636e0f
Vadim Zyamalov <zyamalov.ve@gmail.com>
2021-04-19 06:24:29 +0300

    U: dmenu config.h fix

--- sources/dmenu/config.h/config.h
+++ sources/dmenu/config.h/config.h
@@ -7,7 +7,7 @@ static int centered = 0;                    /* -c option; centers dmenu on scree
 static int min_width = 500;                    /* minimum width when centered */
 /* -fn option overrides fonts[0]; default X11 font or font set */
 static char font[] = "monospace:size=10";
-static char *fonts[] = {
+static const char *fonts[] = {
 	font,
 	"monospace:size=10",
 };
@@ -20,7 +20,7 @@ static char normbgcolor[] = "#222222";
 static char selfgcolor[]  = "#eeeeee";
 static char selbgcolor[]  = "#005577";
 static char highfgcolor[] = "#ffc978";
-static const char *colors[SchemeLast][2] = {
+static char *colors[SchemeLast][2] = {
 	/*     fg         bg       */
 	[SchemeNorm] = { normfgcolor, normbgcolor },
 	[SchemeSel]  = { selfgcolor,  selbgcolor  },
