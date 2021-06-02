bb13b8ebdb0f88063e9f67bda7aeb0f1f5c7d538
Vadim Zyamalov <zyamalov.ve@gmail.com>
2021-06-02 08:19:34 +0300

    U: dwmblocks settings

--- sources/dwmblocks/config.h/blocks.h
+++ sources/dwmblocks/config.h/blocks.h
@@ -1,9 +1,9 @@
 //Modify this file to change what commands output to your statusbar, and recompile using the make command.
 static const Block blocks[] = {
 	/*Icon*/	/*Command*/		/*Update Interval*/	/*Update Signal*/
-	{"",	"~/.local/bin/dwmblocks/newpackages",	900,	8},
-	{"墳",	"~/.local/bin/dwmblocks/volume",	0,	10},
-	{"",	"~/.local/bin/dwmblocks/battery",	5,	0},
+	{"",	"~/.local/bin/dwmblocks/newpackages | xargs printf ' %s'",	900,	8},
+	{"墳",	"~/.local/bin/dwmblocks/volume | xargs printf ' %s'",	0,	10},
+	{"",	"~/.local/bin/dwmblocks/battery | xargs printf ' %s'",	5,	0},
 	{"",	"date '+%H:%M%p' | xargs printf ' %s'",	60,	0},
 };
 
